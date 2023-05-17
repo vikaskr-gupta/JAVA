8eecb16495d737783376341eccdd221fd3a5aefc
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-05-18 01:44:04 +0530

    Add files via upload

---
+++ A2_Example.java
@@ -0,0 +1,11 @@
+class A2_Example 
+{
+    public static void main(String[] args)
+    {
+        int num;
+        num = 100;
+        System.out.println("This is num: " + num);
+        num = num * 2;
+        System.out.println("The value of num * 2 = " + num);
+    }    
+}
