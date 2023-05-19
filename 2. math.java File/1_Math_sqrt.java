fd53ae3aa556a34892db9a25c3ecca35dde69d32
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-05-20 00:59:49 +0530

    Create 1_Math_sqrt.java

---
+++ 2. math.java File/1_Math_sqrt.java
@@ -0,0 +1,9 @@
+class A1_Math_sqrt 
+{
+    public static void main(String[] args)    
+    {
+        double num = 100;
+        double sqrt = Math.sqrt(num);
+        System.out.println(sqrt);
+    }
+}
