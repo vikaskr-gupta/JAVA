2dc93cb787bbb8e079835c76f2347d4fe1583303
Vikas Kumar Gupta <vvikaskgupta86@gmail.com>
2023-05-19 15:49:22 +0530

    Update A_Simle_Class.java

--- Class/A_Simle_Class.java
+++ Class/A_Simle_Class.java
@@ -1,1 +1,19 @@
+class Box
+{
+    double width;
+    double height;
+    double depth;
+}
 
+class A5_A_Simple_Class {
+    public static void main(String[] args)
+    {
+        Box myBox = new Box();
+        double Vol;
+        myBox.width = 3;
+        myBox.height = 5;
+        myBox.depth = 2;
+        Vol = myBox.width * myBox.height * myBox.depth;
+        System.out.println(Vol);
+    } 
+}
